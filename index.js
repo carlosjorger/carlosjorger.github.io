1b037ace256198027a72558c3a02af503cce0aed
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2022-11-16 11:42:48 -0500

    costumize the reveal clases

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function reveal(){
-    var revealElements=document.getElementsByClassName("reveal");
-    [...revealElements].forEach((element)=>{
-        var windowHeight = window.innerHeight;
-        var elementTop = element.getBoundingClientRect().top;
-        console.log(elementTop,windowHeight)
-        if (elementTop < windowHeight - 100) {
-            element.classList.add("active");
-          } else {
-            element.classList.remove("active");
-          }
-    })
+function reveal() {
+  var revealElements = document.getElementsByClassName("reveal");
+  [...revealElements].forEach((element) => {
+    var windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    var extraHeight=( element.classList.contains('footer')?0:70);
+    if (elementTop < windowHeight - (element.getBoundingClientRect().height)-extraHeight) {
+      element.classList.add("active");
+    } else {
+      element.classList.remove("active");
+    }
+  })
 }
-window.addEventListener("scroll",reveal)
+window.addEventListener("scroll", reveal)
