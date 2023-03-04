9660e3117cb0952c6c32a0baa65cf46724d5c714
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-03-03 23:28:53 -0500

    delete class card-project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://carlosjorger.github.io",
+  integrations: [tailwind()]
 });
