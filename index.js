ef89922216a56bb5c01c112834ed9abc4f213a5f
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-03-03 22:03:39 -0500

    create components

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function reveal() {
   var revealElements = document.getElementsByClassName("reveal");
-  [...revealElements].forEach(element => {
+  [...revealElements].forEach((element) => {
     var windowHeight = window.innerHeight;
     const elementTop = element.getBoundingClientRect().top;
     const elementBottom = element.getBoundingClientRect().bottom;
@@ -25,7 +25,7 @@ function changeState(navColumnarMenu, navColumnarMenuBackground) {
     navColumnarMenuBackground.style.visibility = "hidden";
   }
 
-  [...document.getElementsByClassName("changeable")].forEach(element => {
+  [...document.getElementsByClassName("changeable")].forEach((element) => {
     element.classList.toggle("active");
   });
 }
@@ -44,11 +44,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   document.addEventListener(
     "touchstart",
-    ev => {
+    (ev) => {
       const navColumnarMenu = document.getElementById("nav-columnar-menu");
       const navColumnarMenuBackground = document.getElementsByClassName(
         "nav-columnar-menu-background"
       )[0];
+
       if (ev.path[0].classList.contains("nav-columnar-menu-background")) {
         changeState(navColumnarMenu, navColumnarMenuBackground);
       }
