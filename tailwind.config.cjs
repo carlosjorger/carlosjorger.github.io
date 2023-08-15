a09113291a3eb02e1cf4d27068738fc35b895621
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-08-15 15:27:09 -0400

    🦄 refactor: create custom animations
    create custom tailwind animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,19 @@ module.exports = {
     fontFamily: {
       lato: ["Lato", "sans-serif"],
     },
+    keyframes:{
+      'show-presentation':{
+          '0%': {
+            opacity: 0,
+            transform: 'translate(0px, -10px)'
+          }
+      },
+    },
+    animation:{
+      'presentation-title':'500ms show-presentation ease',
+      'presentation-main-skill':'500ms ease 200ms forwards show-presentation',
+      'presentation-description':'500ms ease 400ms forwards show-presentation'
+    }
   },
   plugins: [],
 };
