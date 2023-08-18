792e988f5d3b78dd7b47be7b71d54ebe9228dd31
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-08-18 16:39:37 -0400

    🦄 refactor: use toggle instead
    use toggle instead adding and removing

--- src/utils/scrollEvents.ts
+++ src/utils/scrollEvents.ts
@@ -3,19 +3,13 @@ export function reveal() {
     const windowHeight = window.innerHeight;
     for (const element of revealElements) {
       const {top, bottom} = element.getBoundingClientRect();
-      if (
-        top <= windowHeight - 100 &&
-        bottom >= 100
-      ) {
-        element.classList.remove("inactive");
-      } else {
-        element.classList.add("inactive");
-      }
+      const isActive =  top <= windowHeight - 100 && bottom >= 100;
+      element.classList.toggle("inactive",!isActive)
     }
   }
   export function updateLink(section: HTMLElement,sectionId:string ,limit:number) {
     const {top,bottom} = section.getBoundingClientRect();
-    const isInsideTheSection= top<limit&&bottom>limit;
+    const isInsideTheSection= top<limit && bottom>limit;
     const anchor = document.getElementById(`link-${sectionId}`)
     anchor.classList.toggle('active',isInsideTheSection)
   }
