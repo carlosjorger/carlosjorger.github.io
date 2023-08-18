e6a7f59df726967ae3662dd91ced1f2cb16cedbb
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-08-18 17:18:13 -0400

    ✨ feat: add inactive-down style

--- src/utils/scrollEvents.ts
+++ src/utils/scrollEvents.ts
@@ -1,11 +1,22 @@
 export function reveal() {
     const revealElements = document.getElementsByClassName("reveal");
+    const isScrollingDown = (window.scrollY > (this.lastScrollTop || 0));
     const windowHeight = window.innerHeight;
     for (const element of revealElements) {
       const {top, bottom} = element.getBoundingClientRect();
       const isActive =  top <= windowHeight - 100 && bottom >= 100;
-      element.classList.toggle("inactive",!isActive)
+      const [targetClass,oldclass]=isScrollingDown?
+                                    ["inactive-down","inactive-up"]
+                                    :["inactive-up","inactive-down"]
+                                    
+      if (element.classList.contains(oldclass)) {
+           element.classList.replace(oldclass,targetClass)
+      }
+      else{
+        element.classList.toggle(targetClass,!isActive)
+      }
     }
+    this.lastScrollTop = window.scrollY;
   }
   export function updateLink(section: HTMLElement,sectionId:string ,limit:number) {
     const {top,bottom} = section.getBoundingClientRect();
