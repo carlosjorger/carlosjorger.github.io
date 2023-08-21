beaefcd2c37aec8eb05134fd3b00dad0f69ae964
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-08-21 08:45:26 -0400

    🦄 refactor: refactor reveal function

--- src/utils/scrollEvents.ts
+++ src/utils/scrollEvents.ts
@@ -1,20 +1,22 @@
 // TODO: try to use requestAnimationFrame https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
-export function reveal() {
-	const revealElements = document.getElementsByClassName('reveal');
-	const isScrollingDown = window.scrollY > (this.lastScrollTop || 0);
-	const windowHeight = window.innerHeight;
-	for (const element of revealElements) {
-		const { top, bottom } = element.getBoundingClientRect();
-		const isActive = top <= windowHeight - 100 && bottom >= 100;
-		const [targetClass, oldclass] = isScrollingDown
-			? ['inactive-down', 'inactive-up']
-			: ['inactive-up', 'inactive-down'];
+function hideRevealElement(element: Element, isScrollingDown: boolean) {
+	const { top, bottom } = element.getBoundingClientRect();
+	const isActive = top <= window.innerHeight - 100 && bottom >= 100;
+	const [targetClass, oldclass] = isScrollingDown
+		? ['inactive-down', 'inactive-up']
+		: ['inactive-up', 'inactive-down'];
 
-		if (element.classList.contains(oldclass)) {
-			element.classList.replace(oldclass, targetClass);
-		} else {
-			element.classList.toggle(targetClass, !isActive);
-		}
+	if (element.classList.contains(oldclass)) {
+		element.classList.replace(oldclass, targetClass);
+	} else {
+		element.classList.toggle(targetClass, !isActive);
+	}
+}
+export function hideRevealElements() {
+	const isScrollingDown = window.scrollY > (this.lastScrollTop || 0);
+	const revealElements = document.getElementsByClassName('reveal');
+	for (const element of revealElements) {
+		hideRevealElement(element, isScrollingDown);
 	}
 	this.lastScrollTop = window.scrollY;
 }
