087eef01f6424bd3d86e4383eac25ee12f2d7138
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-11-29 10:29:19 -0500

    ✨ feat: apply scroll-timeline

--- src/utils/scrollEvents.ts
+++ src/utils/scrollEvents.ts
@@ -1,26 +1,3 @@
-// TODO: replace this event with animation-timeline in the future
-// TODO: change animation effect
-function hideRevealElement(element: Element, isScrollingDown: boolean) {
-	const { top, bottom } = element.getBoundingClientRect();
-	const isActive = top <= window.innerHeight - 130 && bottom >= 130;
-	const [targetClass, oldclass] = isScrollingDown
-		? ['inactive-down', 'inactive-up']
-		: ['inactive-up', 'inactive-down'];
-
-	if (element.classList.contains(oldclass)) {
-		element.classList.replace(oldclass, targetClass);
-	} else {
-		element.classList.toggle(targetClass, !isActive);
-	}
-}
-export function hideRevealElements() {
-	const isScrollingDown = window.scrollY > (this.lastScrollTop || 0);
-	const revealElements = document.getElementsByClassName('reveal');
-	for (const element of revealElements) {
-		hideRevealElement(element, isScrollingDown);
-	}
-	this.lastScrollTop = window.scrollY;
-}
 export function activeLinkBySection(section: HTMLElement) {
 	const limit = window.innerHeight * 0.4;
 	const { top, bottom } = section.getBoundingClientRect();
