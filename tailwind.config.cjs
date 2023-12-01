779f42692381e9ad1829e75056cd8e78cd399fe5
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-12-01 16:07:57 -0500

    ✨ feat: add background image to Project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-    fontFamily: {
-      lato: ["Lato", "sans-serif"],
-    },
-    keyframes:{
-      'show-presentation':{
-          '0%': {
-            opacity: 0,
-            transform: 'translate(0px, -10px)'
-          }
-      },
-    },
-    animation:{
-      'presentation-title':'500ms show-presentation ease',
-      'presentation-main-skill':'500ms ease 200ms forwards show-presentation',
-      'presentation-description':'500ms ease 400ms forwards show-presentation'
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				portfolio: "url('./images/Portfolio.webp')",
+				rustycopier: "url('./images/Rustycopier.webp')",
+				cuvimakerExample: "url('./images/Cuvimaker-example.webp')",
+			},
+		},
+		fontFamily: {
+			lato: ['Lato', 'sans-serif'],
+		},
+		keyframes: {
+			'show-presentation': {
+				'0%': {
+					opacity: 0,
+					transform: 'translate(0px, -10px)',
+				},
+			},
+		},
+		animation: {
+			'presentation-title': '500ms show-presentation ease',
+			'presentation-main-skill':
+				'500ms ease 200ms forwards show-presentation',
+			'presentation-description':
+				'500ms ease 400ms forwards show-presentation',
+		},
+	},
+	plugins: [],
 };
