b52ce672da1735f3f754c8d41f9c489e0fba29d8
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-04-03 10:04:23 -0400

    ✨ feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 import compressor from 'astro-compressor';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://carlosjorger.github.io',
-	integrations: [tailwind(), compressor({ gzip: false, brotli: true })],
+	integrations: [
+		tailwind(),
+		compressor({
+			gzip: false,
+			brotli: true,
+		}),
+		sitemap(),
+	],
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en', 'es'],
